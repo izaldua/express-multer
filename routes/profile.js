e8ca979bf0693ad6441f93628be0c2c2580bf68e
izaldua <ikerzalduni@gmail.com>
2024-12-10 10:22:48 +0100

    lehen zatia, izenarena eginik

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Directorio de destino
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const fileExtension = path.extname(file.originalname); // Obtener extensión original
+        cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension); // Guardar con un nombre único
+    }
+});
+
+const upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +26,7 @@ router.post('/', upload.single('avatar'), function (req, res, next) {
     console.log(req.file)
     // req.body will hold the text fields, if there were any
     res.send("Jasota")
-})
+});
 
 
 module.exports = router;
