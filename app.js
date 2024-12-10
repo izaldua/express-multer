c45d9281c078f8f540ca2835e11838e851d0b431
Iker Zaldua <ikerzalduni@gmail.com>
2024-12-10 22:42:35 +0100

    multer ariketa osoa eginik

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs'); // Para manejar el sistema de archivos
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var profileRouter = require('./routes/profile');
 
 var app = express();
+
+// Verificar y crear la carpeta "uploads" si no existe
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Configurar la carpeta "uploads" como estática para servir archivos
+app.use('/uploads', express.static(uploadDir));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/profile.js
+++ routes/profile.js
@@ -12,10 +12,21 @@ const storage = multer.diskStorage({
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         const fileExtension = path.extname(file.originalname); // Obtener extensión original
         cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension); // Guardar con un nombre único
-    }
+    }    
 });
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage, 
+    limits: {
+        fileSize : 2 * 1024 * 1024 //2MB    
+    }, 
+    fileFilter: function(req, file, cb) {
+        if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+            cb(null, true);
+        } else {
+            cb(new Error('PNG edo JPG fitxategiak soilik baimentzen dira'));
+        }
+    }
+ })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,6 +34,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', upload.single('avatar'), function (req, res, next) {
+    const userName = req.body.name;
+    const filePath = `http://localhost:3000/uploads/${req.file.filename}`;
+
+    res.send(`
+        Zure izena: ${userName}, Fitxategia:  <a href="${filePath}">${filePath}</a>
+        <br>
+        <img src="${filePath}" alt="Irudia" style="max-width: 300px; max-height: 300px;">
+    `);
+
     console.log(req.file)
     // req.body will hold the text fields, if there were any
     res.send("Jasota")
